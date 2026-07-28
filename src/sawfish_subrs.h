5d1445d1e09e5ddb45a04111f578507bcaaa9c6e
Michal Maruska <mmaruska@gmail.com>
2026-07-28 22:33:03 +0200

    declare mark_all_frame_parts_as_exposed for others to use

--- src/sawfish_subrs.h
+++ src/sawfish_subrs.h
@@ -142,6 +142,7 @@ extern struct frame_part *find_frame_part_by_window (Window id);
 extern void frame_part_exposer (XExposeEvent *ev, struct frame_part *fp);
 extern repv get_keymap_for_frame_part (struct frame_part *fp);
 extern void mark_frame_parts (Lisp_Window *w);
+extern void mark_all_frame_parts_as_exposed(Lisp_Window *w);
 extern void reset_frame_parts (Lisp_Window *w);
 extern void restack_frame_parts (Lisp_Window *w);
 extern repv Vframe_draw_mutex (repv arg);
